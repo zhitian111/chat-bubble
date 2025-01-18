2e7dd56665806524a53bf698a3e2f9d217ab511e
zhitian111 <167742129+zhitian111@users.noreply.github.com>
2025-01-18 13:44:19 +0800

    chat v9

--- choose_sentence/choose_sentence.gd
+++ choose_sentence/choose_sentence.gd
@@ -4,9 +4,9 @@ extends VBoxContainer
 @onready var button_2 = $CenterContainer2/Button
 @onready var button_3 = $CenterContainer3/Button
 
-@onready var text1 = $CenterContainer/HBoxContainer/Label2.text
-@onready var text2 = $CenterContainer2/HBoxContainer/Label2.text
-@onready var text3 = $CenterContainer3/HBoxContainer/Label2.text
+@onready var text1 = $CenterContainer/HBoxContainer/Label2
+@onready var text2 = $CenterContainer2/HBoxContainer/Label2
+@onready var text3 = $CenterContainer3/HBoxContainer/Label2
 
 func _ready() -> void:
 	button_1.pressed.connect(chosen)
