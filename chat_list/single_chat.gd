91e5b8f17568ab7ae85fecc3f66dbc2a460ea1ac
kasssssssssss <1592858973@qq.com>
2025-01-18 10:45:05 +0800

    233

--- chat_list/single_chat.gd
+++ chat_list/single_chat.gd
@@ -2,9 +2,10 @@ extends Control
 @onready var avatar = $HBoxContainer/avatar
 @onready var chat_name = $HBoxContainer/VBoxContainer/name
 @onready var message = $HBoxContainer/VBoxContainer/message
-@onready var time = $HBoxContainer/VBoxContainer2/time
+@onready var time = $TimerBox/time
 @onready var red_point=$HBoxContainer/avatar/Sprite2D
 @onready var red_point_num=$HBoxContainer/avatar/Sprite2D/Label
+@onready var timer_box=$TimerBox
 var timer: Timer
 var number:int=0
 signal chat_button_pressed
@@ -13,6 +14,7 @@ func _ready() -> void:
 	red_point.visible=false
 	red_point_num.visible=false
 	timer = Timer.new()
+	add_child(timer)
 	timer.set_wait_time(5)
 func init(path: String, aname: String, amessage: String) -> void:
 	avatar.texture = load(path)
@@ -21,16 +23,20 @@ func init(path: String, aname: String, amessage: String) -> void:
 		amessage = amessage.substr(0, 7) + "..."  # 超过部分显示省略号
 	message.text = amessage
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+func set_message(amessage:String):
+	message.text = amessage
 func set_timer(atime: float=5):
-	add_child(timer)
+	timer.stop()
 	timer.set_wait_time(atime)
 	timer.start()
+	timer_box.visible=true
 func _on_timer_timeout() -> void:
 	emit_signal("time_end")
+	timer_box.visible=false
 
 # 更新剩余时间
 func _process(_delta: float) -> void:
-	time.text = "剩余时间：" + str(timer.time_left)
+	time.text = "剩余时间：" + str(int(timer.time_left))
 
 
 func _on_button_button_down() -> void:
