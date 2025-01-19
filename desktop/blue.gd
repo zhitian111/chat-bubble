325549735c9124b23518aefd1a714118007b2b27
zhitian111 <167742129+zhitian111@users.noreply.github.com>
2025-01-19 11:46:08 +0800

    animation_v3

--- desktop/blue.gd
+++ desktop/blue.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+var can_back:bool = false
+
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://desktop/desktop.tscn")
 	print("重启")
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed && can_back:
+			_on_button_button_down()
+
+func end_animation():
+	can_back = true
